1a2e4d71567e008f939ae2724e15abe956f81beb
severalgh <pdrzycimski@gmail.com>
2018-06-04 15:12:06 +0200

    Refactor HasUT to use value parametrized test technique.

--- test/HashUT.cpp
+++ test/HashUT.cpp
@@ -11,123 +11,92 @@ namespace so { namespace ut { namespace hash {
 
 namespace hash = ::so::hash;
 
-TEST(Hash, md4)
+struct HashTestInput
 {
-  // GIVEN
-  // generated using 'openssl md4' on debian
-  const std::string str = "test_test_foobar";
-  const std::string expectedHexString = "e0e7ea8d8da4cd38dea5c40951292dae";
-  ::so::Bytes data(str.size());
-  std::transform(str.begin(), str.end(), data.begin(), [](char chr){return static_cast<uint8_t>(chr);});
+  std::string shortDesc;
+  std::string plaintext;
+  std::string expectedHexString;
+  std::function<::so::Expected<::so::Bytes>(const ::so::Bytes&)> hasher;
+};
 
-  // WHEN
-  const auto hash = hash::md4(data);
-
-  // THEN
-  ASSERT_TRUE(hash);
-  EXPECT_EQ(expectedHexString, utils::bin2Hex(*hash));
+std::ostream& operator<<(std::ostream &s, const HashTestInput &i)
+{
+  return s << i.shortDesc;
 }
 
-TEST(Hash, md5)
+class HashUT: public ::testing::TestWithParam<HashTestInput>
+{};
+
+TEST_P(HashUT, hash)
 {
-  // GIVEN
-  // generated using debian's builtin md5sum
-  const std::string str = "test_test_foobar";
-  const std::string expectedHexString = "506c5777af0c699d27f0e1214343e90a";
-  ::so::Bytes data(str.size());
-  std::transform(str.begin(), str.end(), data.begin(), [](char chr){return static_cast<uint8_t>(chr);});
+  const HashTestInput input { GetParam() };
+  
+  ::so::Bytes data(input.plaintext.size());
+  std::transform(input.plaintext.begin(), input.plaintext.end(), data.begin(), [](char chr){return static_cast<uint8_t>(chr);});
 
   // WHEN
-  const auto hash = hash::md5(data);
+  const auto hash = input.hasher(data);
 
   // THEN
   ASSERT_TRUE(hash);
-  EXPECT_EQ(expectedHexString, utils::bin2Hex(*hash));
+  EXPECT_EQ(input.expectedHexString, utils::bin2Hex(*hash));
 }
 
-TEST(Hash, sha1)
-{
-  // GIVEN
-  // generated using debian's builtin sha1sum
-  const std::string str = "test_test_foobar";
-  const std::string expectedHexString = "42316b3d7b91ddb03e3980173e66d59522ceafb0";
-  ::so::Bytes data(str.size());
-  std::transform(str.begin(), str.end(), data.begin(), [](char chr){return static_cast<uint8_t>(chr);});
+const auto testCases = ::testing::Values(
 
-  // WHEN
-  const auto hash = hash::sha1(data);
+    HashTestInput{
+      "md4",
+      "test_test_foobar",
+      "e0e7ea8d8da4cd38dea5c40951292dae",
+      ::so::hash::md4
+    },
 
-  // THEN
-  ASSERT_TRUE(hash);
-  EXPECT_EQ(expectedHexString, utils::bin2Hex(*hash));
-}
+  HashTestInput{
+      "md5",
+      "test_test_foobar",
+      "506c5777af0c699d27f0e1214343e90a",
+      ::so::hash::md5
+    },
 
-TEST(Hash, sha224)
-{
-  // GIVEN
-  // generated using debian's builtin sha224sum
-  const std::string str = "test_test_foobar";
-  const std::string expectedHexString = "42388802eda260242eabb109f3959dca377cc326a4fe6ef0b6cf7b25";
-  ::so::Bytes data(str.size());
-  std::transform(str.begin(), str.end(), data.begin(), [](char chr){return static_cast<uint8_t>(chr);});
+  HashTestInput{
+      "sha1",
+      "test_test_foobar",
+      "42316b3d7b91ddb03e3980173e66d59522ceafb0",
+      ::so::hash::sha1
+    },
 
-  // WHEN
-  const auto hash = hash::sha224(data);
+  HashTestInput{
+      "sha224",
+      "test_test_foobar",
+      "42388802eda260242eabb109f3959dca377cc326a4fe6ef0b6cf7b25",
+      ::so::hash::sha224
+    },
 
-  // THEN
-  ASSERT_TRUE(hash);
-  EXPECT_EQ(expectedHexString, utils::bin2Hex(*hash));
-}
+  HashTestInput{
+      "sha256",
+      "test_test_foobar",
+      "bc454e8c3505ee0315b34389de085393a08e0eb7f5d600222207bbe3498c53a6",
+      ::so::hash::sha256
+    },
 
-TEST(Hash, sha256)
-{
-  // GIVEN
-  // generated using debian's builtin sha256sum
-  const std::string str = "test_test_foobar";
-  const std::string expectedHexString = "bc454e8c3505ee0315b34389de085393a08e0eb7f5d600222207bbe3498c53a6";
-  ::so::Bytes data(str.size());
-  std::transform(str.begin(), str.end(), data.begin(), [](char chr){return static_cast<uint8_t>(chr);});
+  HashTestInput{
+      "sha384",
+      "test_test_foobar",
+      "9962cbe6aae7259c26a50f1892bcfab6b287ad0609a484739a2ac6d0f3e800533b369b663fd6ddc0bd7d3dee62801d4b",
+      ::so::hash::sha384
+    },
 
-  // WHEN
-  const auto hash = hash::sha256(data);
+  HashTestInput{
+      "sha512",
+      "test_test_foobar",
+      "781a5cfc1fb0cb51b550696fad366c37486000a69f70510701106db7e8081a381c0790f245e14cfbbb1eaee59c1e8811dc05d82cf75f50b7eabc1ff7923b0a08",
+      ::so::hash::sha512
+    }
+);
 
-  // THEN
-  ASSERT_TRUE(hash);
-  EXPECT_EQ(expectedHexString, utils::bin2Hex(*hash));
-}
-
-TEST(Hash, sha384)
-{
-  // GIVEN
-  // generated using debian's builtin sha384sum
-  const std::string str = "test_test_foobar";
-  const std::string expectedHexString = "9962cbe6aae7259c26a50f1892bcfab6b287ad0609a484739a2ac6d0f3e800533b369b663fd6ddc0bd7d3dee62801d4b";
-  ::so::Bytes data(str.size());
-  std::transform(str.begin(), str.end(), data.begin(), [](char chr){return static_cast<uint8_t>(chr);});
-
-  // WHEN
-  const auto hash = hash::sha384(data);
-
-  // THEN
-  ASSERT_TRUE(hash);
-  EXPECT_EQ(expectedHexString, utils::bin2Hex(*hash));
-}
-
-TEST(Hash, sha512)
-{
-  // GIVEN
-  // generated using debian's builtin sha512sum
-  const std::string str = "test_test_foobar";
-  const std::string expectedHexString = "781a5cfc1fb0cb51b550696fad366c37486000a69f70510701106db7e8081a381c0790f245e14cfbbb1eaee59c1e8811dc05d82cf75f50b7eabc1ff7923b0a08";
-  ::so::Bytes data(str.size());
-  std::transform(str.begin(), str.end(), data.begin(), [](char chr){return static_cast<uint8_t>(chr);});
-
-  // WHEN
-  const auto hash = hash::sha512(data);
-
-  // THEN
-  ASSERT_TRUE(hash);
-  EXPECT_EQ(expectedHexString, utils::bin2Hex(*hash));
-} 
-
-}}} // namespace so { namespace ut { namepsace hash
+INSTANTIATE_TEST_CASE_P(
+    Hash,
+    HashUT,
+    testCases 
+);
+}}}
