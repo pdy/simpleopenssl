d6e99e111fc2da4c860ebd70394d485847abe2d3
Pawel Drzycimski <pdrzycimski@gmail.com>
2018-11-12 21:33:04 +0100

    Add tests to ecdsa signature format converters

--- Makefile
+++ Makefile
@@ -47,8 +47,8 @@ strip:
 	$(STRIP) $(DESTBIN)/*
 
 OBJS_UT := $(OBJ)/Asn1UT.o $(OBJ)/EcdsaSignVerifyUT.o $(OBJ)/EvpSignVerifyUT.o $(OBJ)/EcdsaKeyUT.o \
-	$(OBJ)/EcdsaKeyUT.o $(OBJ)/EcdsaKeyGenUT.o $(OBJ)/EvpKeyUT.o $(OBJ)/HashUT.o $(OBJ)/X509UT.o \
-	$(OBJ)/X509PemUT.o $(OBJ)/X509CertExtensionsUT.o $(OBJ)/BignumUT.o
+	$(OBJ)/EcdsaKeyUT.o $(OBJ)/EcdsaKeyGenUT.o $(OBJ)/EcdsaSignatureConvertersUT.o $(OBJ)/EvpKeyUT.o \
+	$(OBJ)/HashUT.o $(OBJ)/X509UT.o $(OBJ)/X509PemUT.o $(OBJ)/X509CertExtensionsUT.o $(OBJ)/BignumUT.o
 
 $(DESTBIN)/UnitTests: $(OBJS_UT)
 	$(CXX) $(CXXFLAGS) $(TEST_FLAGS) -o $@ $^ $(LD_FLAGS) $(GTEST_LIBS) $(LD_LIBS)
---
+++ test/EcdsaSignatureConvertersUT.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+#include <simpleopenssl/simpleopenssl.h>
+
+#include "precalculated.h"
+#include "utils.h"
+
+namespace so { namespace ut { namespace ecdsa {
+
+namespace ecdsa = ::so::ecdsa;
+
+TEST(ecdsa, derSignatureBytesToSignatureStruct)
+{
+  // GIVEN
+  const ecdsa::Signature expected { data::signature_sha256_R, data::signature_sha256_S };
+
+  // WHEN
+  auto maybeSig = ecdsa::derToSignature(data::signature_sha256);
+  ASSERT_TRUE(maybeSig);
+  
+  auto sig = maybeSig.moveValue();
+
+  // THEN
+  EXPECT_EQ(expected, sig); 
+}
+
+TEST(ecdsa, signatureStructToDerBytes)
+{
+  // GIVEN
+  const so::Bytes expected { data::signature_sha256 };
+  const ecdsa::Signature sig { data::signature_sha256_R, data::signature_sha256_S };
+
+  // WHEN
+  auto maybeDer = ecdsa::signatureToDer(sig);
+  ASSERT_TRUE(maybeDer);
+
+  auto der = maybeDer.moveValue();
+
+  // THEN
+  ASSERT_EQ(expected.size(), der.size());
+  EXPECT_TRUE(std::equal(expected.begin(), expected.end(), der.begin()));
+}
+
+}}} // namespace so { namespace ut { namespace ecdsa {
