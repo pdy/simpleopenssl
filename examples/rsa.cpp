127ac48810c58a428ff5adfcdab91aa8b3373d15
severalgh <pdrzycimski@gmail.com>
2019-11-22 17:21:55 +0100

    Add examples folder with Makefile and apps to implement.

---
+++ examples/rsa.cpp
@@ -0,0 +1,10 @@
+#include "cmdline/cmdline.h"
+#include <simpleopenssl/simpleopenssl.h>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  std::cout << "Hello rsa program!\n";
+  return 0;
+}
