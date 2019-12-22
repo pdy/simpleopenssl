4988dbedbb5080fa2befdb201c37283325c9a8f1
Pawel Drzycimski <pdrzycimski@gmail.com>
2019-12-22 23:07:45 +0100

    Add assertions in UTs for some Expected<> specializations size.

--- test/ExpectedUT.cpp
+++ test/ExpectedUT.cpp
@@ -11,7 +11,7 @@ TEST(ExpectedUT, okUsageWithBytes)
 {
   // WHEN
   const auto expected = ::so::internal::ok(::so::Bytes{0x00, 0x01});
-
+ 
   // THEN
   EXPECT_TRUE(expected);
   EXPECT_TRUE(expected.hasValue());
@@ -20,12 +20,16 @@ TEST(ExpectedUT, okUsageWithBytes)
   EXPECT_EQ("ok", expected.msg());
   EXPECT_EQ((::so::Bytes{0x00, 0x01}), *expected);
   EXPECT_EQ((::so::Bytes{0x00, 0x01}), expected.value());
+  EXPECT_EQ(32, sizeof(expected));
 }
 
 TEST(ExpectedUT, errUsageWithBytes)
 {
   // WHEN
   const auto expected = ::so::internal::err<::so::Bytes>(5);
+
+  const auto szExp = sizeof expected;
+  (void)szExp;
 
   // THEN
   EXPECT_FALSE(expected);
@@ -34,6 +38,7 @@ TEST(ExpectedUT, errUsageWithBytes)
   EXPECT_EQ(static_cast<unsigned long>(5), expected.errorCode());
   EXPECT_EQ((::so::Bytes{}), *expected);
   EXPECT_EQ((::so::Bytes{}), expected.value());
+  EXPECT_EQ(32, sizeof(expected));
 }
 
 TEST(ExpectedUT, okUsageWithUptrs)
@@ -47,40 +52,44 @@ TEST(ExpectedUT, okUsageWithUptrs)
   EXPECT_FALSE(expected.hasError());
   EXPECT_EQ(static_cast<unsigned long>(0), expected.errorCode());
   EXPECT_EQ("ok", expected.msg());
+  EXPECT_EQ(16, sizeof(expected));
 }
 
 TEST(ExpectedUT, errUsageWithUptrs)
 {
   // WHEN 
   const auto expected = ::so::internal::err<::so::BIGNUM_uptr>(5);
-
+ 
   // THEN
   EXPECT_FALSE(expected);
   EXPECT_FALSE(expected.hasValue());
   EXPECT_TRUE(expected.hasError());
   EXPECT_EQ(static_cast<unsigned long>(5), expected.errorCode());
+  EXPECT_EQ(16, sizeof(expected));
 }
 
 TEST(ExpectedUT, okUsageWithVoid)
 {
   // WHEN
-  const auto expected = ::so::internal::ok();
-  
+  const auto expected = ::so::internal::ok(); 
+
   // THEN  
   EXPECT_TRUE(expected);
   EXPECT_FALSE(expected.hasError());
   EXPECT_EQ(static_cast<unsigned long>(0), expected.errorCode());
   EXPECT_EQ("ok", expected.msg());
+  EXPECT_EQ(8, sizeof(expected));
 }
 
 TEST(ExpectedUT, errUsageWithVoid)
 {
   // WHEN  
   const auto expected = ::so::internal::err<void>(5);
-
+ 
   // THEN
   EXPECT_FALSE(expected);
   EXPECT_TRUE(expected.hasError());
   EXPECT_EQ(static_cast<unsigned long>(5), expected.errorCode());
+  EXPECT_EQ(8, sizeof(expected));
 }
 }}} // namespace so { namespace ut { namespace bignum {
