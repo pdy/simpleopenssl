562f8a77311268602919a27c32a7f99786574eb6
Pawel Drzycimski <pdrzycimski@gmail.com>
2019-12-23 01:25:07 +0100

    Disable Expected size checks in UTs.

--- test/ExpectedUT.cpp
+++ test/ExpectedUT.cpp
@@ -20,7 +20,7 @@ TEST(ExpectedUT, okUsageWithBytes)
   EXPECT_EQ("ok", expected.msg());
   EXPECT_EQ((::so::Bytes{0x00, 0x01}), *expected);
   EXPECT_EQ((::so::Bytes{0x00, 0x01}), expected.value());
-  EXPECT_EQ(32, sizeof(expected));
+  //EXPECT_EQ(32, sizeof(expected));
 }
 
 TEST(ExpectedUT, errUsageWithBytes)
@@ -38,7 +38,7 @@ TEST(ExpectedUT, errUsageWithBytes)
   EXPECT_EQ(static_cast<unsigned long>(5), expected.errorCode());
   EXPECT_EQ((::so::Bytes{}), *expected);
   EXPECT_EQ((::so::Bytes{}), expected.value());
-  EXPECT_EQ(32, sizeof(expected));
+  //EXPECT_EQ(32, sizeof(expected));
 }
 
 TEST(ExpectedUT, okUsageWithUptrs)
@@ -52,7 +52,7 @@ TEST(ExpectedUT, okUsageWithUptrs)
   EXPECT_FALSE(expected.hasError());
   EXPECT_EQ(static_cast<unsigned long>(0), expected.errorCode());
   EXPECT_EQ("ok", expected.msg());
-  EXPECT_EQ(16, sizeof(expected));
+  //EXPECT_EQ(16, sizeof(expected));
 }
 
 TEST(ExpectedUT, errUsageWithUptrs)
@@ -65,7 +65,7 @@ TEST(ExpectedUT, errUsageWithUptrs)
   EXPECT_FALSE(expected.hasValue());
   EXPECT_TRUE(expected.hasError());
   EXPECT_EQ(static_cast<unsigned long>(5), expected.errorCode());
-  EXPECT_EQ(16, sizeof(expected));
+  //EXPECT_EQ(16, sizeof(expected));
 }
 
 TEST(ExpectedUT, okUsageWithVoid)
@@ -78,7 +78,7 @@ TEST(ExpectedUT, okUsageWithVoid)
   EXPECT_FALSE(expected.hasError());
   EXPECT_EQ(static_cast<unsigned long>(0), expected.errorCode());
   EXPECT_EQ("ok", expected.msg());
-  EXPECT_EQ(8, sizeof(expected));
+  //EXPECT_EQ(8, sizeof(expected));
 }
 
 TEST(ExpectedUT, errUsageWithVoid)
@@ -90,6 +90,6 @@ TEST(ExpectedUT, errUsageWithVoid)
   EXPECT_FALSE(expected);
   EXPECT_TRUE(expected.hasError());
   EXPECT_EQ(static_cast<unsigned long>(5), expected.errorCode());
-  EXPECT_EQ(8, sizeof(expected));
+  //EXPECT_EQ(8, sizeof(expected));
 }
 }}} // namespace so { namespace ut { namespace bignum {
