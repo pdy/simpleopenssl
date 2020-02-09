a641b2f9839a7039c60c15b2111586180a14c94a
Pawel Drzycimski <pdrzycimski@gmail.com>
2020-02-09 22:19:27 +0100

    Saving minor changes.

--- examples/x509info.cpp
+++ examples/x509info.cpp
@@ -1,12 +1,18 @@
 #include <simpleopenssl/simpleopenssl.h>
 #include "simplelog/simplelog.h"
 #include "cmdline/cmdline.h"
+#include <iomanip>
+
+
+using namespace so;
+
+std::string bin2Hex(const so::Bytes &buff);
 
 int main(int argc, char *argv[])
 {
   cmdline::parser arg;
   arg.add("help", 'h', "Print help.");
-  arg.add<std::string>("file", 'f', "File to be loaded.", true);
+  arg.add<std::string>("file", 'f', "PEM cert file to be printed.", true);
  
   if(!arg.parse(argc, const_cast<const char* const*>(argv)))
   {
@@ -30,8 +36,18 @@ int main(int argc, char *argv[])
     log << arg.usage();
     return 0;
   }
-
-//  const std::string file = arg.get<std::string>("file");
+  
+  //const std::string file = arg.get<std::string>("file");
+//  auto maybeX509 = x509::verifySignature
 
   return 0;
+}
+
+std::string bin2Hex(const so::Bytes &buff)
+{
+  std::ostringstream oss;
+  for(const auto bt : buff){
+    oss << std::setfill('0') << std::setw(2) << std::hex << +bt;
+  }
+  return oss.str(); 
 }
