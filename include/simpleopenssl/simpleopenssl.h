69eba8b683bcd841fa17e7c396f9703842726025
Pawel Drzycimski <pdrzycimski@gmail.com>
2021-06-27 00:49:36 +0200

    Minor update in copyright header

--- include/simpleopenssl/simpleopenssl.h
+++ include/simpleopenssl/simpleopenssl.h
@@ -4,7 +4,7 @@
 /*
 *  MIT License
 *  
-*  Copyright (c) 2018 Pawel Drzycimski
+*  Copyright (c) 2018-2021 Pawel Drzycimski
 *  
 *  Permission is hereby granted, free of charge, to any person obtaining a copy
 *  of this software and associated documentation files (the "Software"), to deal
