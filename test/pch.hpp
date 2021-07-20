c36dc99b4d1429899c60fdc9e4922df1e06a1478
Pawel Drzycimski <pdrzycimski@gmail.com>
2021-07-20 12:01:24 +0200

    Use simpleopenssl header in test pch.hpp instead of separate openssl and std includes

--- test/pch.hpp
+++ test/pch.hpp
@@ -4,31 +4,10 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-// openssl
-#include <openssl/asn1.h>
-#include <openssl/ec.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
-#include <openssl/bio.h>
-#include <openssl/bn.h>
-#include <openssl/x509.h>
-#include <openssl/x509v3.h>
-#include <openssl/pem.h>
-#include <openssl/cmac.h>
-#include <openssl/rand.h>
-#include <openssl/md5.h>
-#include <openssl/md4.h>
-
-// std
-#include <vector>
-#include <memory>
-#include <type_traits>
-#include <algorithm>
-#include <cstring>
-#include <chrono>
-#include <iterator>
-
 // so
+#include <simpleopenssl/simpleopenssl.h>
+
+// so ut
 #include "precalculated.h"
 #include "utils.h"
 
