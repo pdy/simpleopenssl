eeef9b9cac2e3f86c5f3688d90ef91323c30e4ca
Pawel Drzycimski <pdrzycimski@gmail.com>
2021-07-28 23:27:43 +0200

    Do not use simpleopenssl.h header in test pch.hpp to not be forced to recompile it every time I add something to library

--- test/pch.hpp
+++ test/pch.hpp
@@ -4,8 +4,30 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-// so
-#include <simpleopenssl/simpleopenssl.h>
+// openssl
+#include <openssl/asn1.h>
+#include <openssl/ec.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/bio.h>
+#include <openssl/bn.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+#include <openssl/pem.h>
+#include <openssl/cmac.h>
+#include <openssl/rand.h>
+#include <openssl/md5.h>
+#include <openssl/md4.h>
+
+// std
+#include <vector>
+#include <memory>
+#include <type_traits>
+#include <algorithm>
+#include <cstring>
+#include <chrono>
+#include <iterator>
+#include <sstream>
 
 // so ut
 #include "precalculated.h"
